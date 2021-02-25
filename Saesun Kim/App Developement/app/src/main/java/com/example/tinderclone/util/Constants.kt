bfff52ba2f43e09d9f41615be780ddbdde39e337
Saesun Kim <59627358+bagmk@users.noreply.github.com>
2021-02-25 03:08:58 -0600

    Profile

--- Saesun Kim/App Developement/app/src/main/java/com/example/tinderclone/util/Constants.kt
+++ Saesun Kim/App Developement/app/src/main/java/com/example/tinderclone/util/Constants.kt
@@ -1,3 +1,13 @@
 package com.example.tinderclone.util
 
 val Data_USERS = "Users"
+
+val DATA_NAME="name"
+
+val DATA_AGE="age"
+val DATA_GENDER="gender"
+val DATA_EMAIL="email"
+val DATA_GENDER_PREFERENCE="preferredGender"
+val GENDER_MALE ="male"
+val GENDER_FEMALE ="female"
+val DATA_IMAGE_URL ="imageUrl"
