f43d02f1e83f319e94ebefbe42546248c85cea24
Saesun Kim <59627358+bagmk@users.noreply.github.com>
2021-02-26 01:17:25 -0600

    Finally!

--- Saesun Kim/App Developement/app/src/main/java/com/example/tinderclone/fragments/SwipeFragment.kt
+++ Saesun Kim/App Developement/app/src/main/java/com/example/tinderclone/fragments/SwipeFragment.kt
@@ -9,22 +9,36 @@ import android.widget.ArrayAdapter
 import com.example.tinderclone.R
 import com.example.tinderclone.User
 import com.example.tinderclone.activity.TinderCallback
+import com.example.tinderclone.adapters.CardsAdapter
+import com.example.tinderclone.util.DATA_GENDER
+import com.example.tinderclone.util.DATA_MATCHES
+import com.example.tinderclone.util.DATA_SWIPES_LEFT
+import com.example.tinderclone.util.DATA_SWIPES_RIGHT
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.lorentzos.flingswipe.SwipeFlingAdapterView
+import kotlinx.android.synthetic.main.fragment_profile.view.*
+import kotlinx.android.synthetic.main.fragment_swipe.*
 
 
 class SwipeFragment : Fragment() {
     private var callback: TinderCallback? = null
-
-    private lateinit var userId:String
+    private lateinit var userId: String
     private lateinit var userDatabase: DatabaseReference
-    private var cardsAdapter: ArrayAdapter<User>?=null
+    private var cardsAdapter: ArrayAdapter<User>? = null
     private var rowItems = ArrayList<User>()
 
+    private var preferredGender: String? = null
+
+
     fun setCallback(callback: TinderCallback) {
-        this.callback=callback
-        userId=callback.onGetUserId()
-        userDatabase=callback.getUserDatabase()
+        this.callback = callback
+        userId = callback.onGetUserId()
+        userDatabase = callback.getUserDatabase()
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -36,6 +50,71 @@ class SwipeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        cardsAdapter =
+        userDatabase.child(userId).addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot) {
+                val user = p0.getValue(User::class.java)
+                preferredGender = user?.preferredGender
+                populateItems()
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+        })
+
+
+        cardsAdapter = CardsAdapter(context, R.layout.item, rowItems)
+
+        frame.adapter = cardsAdapter
+        frame.setFlingListener(object : SwipeFlingAdapterView.onFlingListener {
+            override fun removeFirstObjectInAdapter() {
+            }
+
+            override fun onLeftCardExit(p0: Any?) {
+            }
+
+            override fun onRightCardExit(p0: Any?) {
+            }
+
+            override fun onAdapterAboutToEmpty(p0: Int) {
+            }
+
+            override fun onScroll(p0: Float) {
+            }
+        })
     }
+
+    fun populateItems() {
+        noUsersLayout.visibility = View.GONE
+        progressLayout.visibility = View.VISIBLE
+        val cardsQuery = userDatabase.orderByChild(DATA_GENDER).equalTo(preferredGender)
+        cardsQuery.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                p0.children.forEach { child ->
+                    val user = child.getValue(User::class.java)
+                    if (user != null) {
+                        var showUser = true
+                        if (child.child(DATA_SWIPES_LEFT).hasChild(userId) ||
+                            child.child(DATA_SWIPES_RIGHT).hasChild(userId) ||
+                            child.child(DATA_MATCHES).hasChild(userId)
+                        ) {
+                            showUser = false
+                        }
+                        if (showUser) {
+                            rowItems.add(user)
+                            cardsAdapter?.notifyDataSetChanged()
+                        }
+                    }
+                }
+                progressLayout.visibility = View.GONE
+                if (rowItems.isEmpty()) {
+                    noUsersLayout.visibility = View.VISIBLE
+                }
+            }
+        })
+    }
+
 }
--- Saesun Kim/App Developement/app/src/main/java/com/example/tinderclone/util/Constants.kt
+++ Saesun Kim/App Developement/app/src/main/java/com/example/tinderclone/util/Constants.kt
@@ -11,3 +11,7 @@ val DATA_GENDER_PREFERENCE="preferredGender"
 val GENDER_MALE ="male"
 val GENDER_FEMALE ="female"
 val DATA_IMAGE_URL ="imageUrl"
+
+val DATA_SWIPES_LEFT = "swipesLeft"
+val DATA_SWIPES_RIGHT = "swipesRight"
+val DATA_MATCHES = "matches"
