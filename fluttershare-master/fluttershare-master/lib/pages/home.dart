ffe3d5388d772d848e6017da8b9ec4f6fc892b8f
Saesun Kim <59627358+bagmk@users.noreply.github.com>
2021-04-11 21:03:45 -0600

    upload

--- fluttershare-master/fluttershare-master/lib/pages/home.dart
+++ fluttershare-master/fluttershare-master/lib/pages/home.dart
@@ -7,8 +7,52 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool isAuth = false;
+
+  Widget buildAuthScreen() {
+    return Text('Authenticated');
+  }
+
+  Scaffold buildUnAuthScreen() {
+    return Scaffold(
+      body: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [Colors.teal, Colors.purple])),
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Barter',
+                style: TextStyle(
+                  fontFamily: "Signatra",
+                  fontSize: 90.0,
+                  color: Colors.white,
+                ),
+              ),
+              GestureDetector(
+                onTap: () => print('tapped'),
+                child: Container(
+                  width: 260.0,
+                  height: 60.0,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage(
+                              'assets/images/google_signin_button.png'),
+                          fit: BoxFit.cover)),
+                ),
+              )
+            ],
+          )),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("Home");
+    return isAuth ? buildAuthScreen() : buildUnAuthScreen();
   }
 }
