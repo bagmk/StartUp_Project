afcf7729f9d419e5034a37e33bb3c69c5c5b0120
Saesun <saesun.kim-1@ou.edu>
2021-04-17 23:27:21 -0500

    asd

--- fluttershare-master/fluttershare-master/lib/pages/timeline.dart
+++ fluttershare-master/fluttershare-master/lib/pages/timeline.dart
@@ -1,9 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttershare/widgets/header.dart';
 import 'package:fluttershare/widgets/progress.dart';
-
-final usersRef = FirebaseFirestore.instance.collection('users');
 
 class Timeline extends StatefulWidget {
   @override
@@ -11,21 +8,6 @@ class Timeline extends StatefulWidget {
 }
 
 class _TimelineState extends State<Timeline> {
-  void initState() {
-    getUsers();
-    super.initState();
-  }
-
-  getUsers() {
-    usersRef.get().then((QuerySnapshot snapshot) {
-      snapshot.docs.forEach((DocumentSnapshot doc) {
-        print(doc.data);
-        print(doc.id);
-        print(doc.exists);
-      });
-    });
-  }
-
   @override
   Widget build(context) {
     return Scaffold(
