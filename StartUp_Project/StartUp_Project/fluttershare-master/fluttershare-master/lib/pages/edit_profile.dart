a00f3f1f262f6907298ea0618436e27bc9cc23c5
Saesun <kimjr001@umn.edu>
2021-04-25 20:56:39 -0500

    asdf

--- StartUp_Project/StartUp_Project/fluttershare-master/fluttershare-master/lib/pages/edit_profile.dart
+++ StartUp_Project/StartUp_Project/fluttershare-master/fluttershare-master/lib/pages/edit_profile.dart
@@ -1,13 +1,177 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import "package:flutter/material.dart";
+import 'package:fluttershare/models/user.dart';
+import 'package:fluttershare/pages/home.dart';
+import 'package:fluttershare/widgets/progress.dart';
 
 class EditProfile extends StatefulWidget {
+  final String currentUserId;
+
+  EditProfile({this.currentUserId});
+
   @override
   _EditProfileState createState() => _EditProfileState();
 }
 
 class _EditProfileState extends State<EditProfile> {
+  TextEditingController displayNameController = TextEditingController();
+  TextEditingController bioController = TextEditingController();
+  bool isLoading = false;
+  User user;
+  bool _bioValid = true;
+  bool _displayNameValid = true;
+  final _scaffoldkey = GlobalKey<ScaffoldState>();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getUser();
+  }
+
+  getUser() async {
+    setState(() {
+      isLoading = true;
+    });
+    DocumentSnapshot doc = await usersRef.doc(widget.currentUserId).get();
+    user = User.fromDocument(doc);
+    displayNameController.text = user.displayName;
+    bioController.text = user.bio;
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  buildDisplayNameField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 12.0),
+          child: Text(
+            "Display Name",
+            style: TextStyle(color: Colors.grey),
+          ),
+        ),
+        TextField(
+          controller: displayNameController,
+          decoration: InputDecoration(
+              hintText: "Update Display Name",
+              errorText: _displayNameValid ? null : "Display Name too short"),
+        )
+      ],
+    );
+  }
+
+  buildBioField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 12.0),
+          child: Text(
+            "Bio",
+            style: TextStyle(color: Colors.grey),
+          ),
+        ),
+        TextField(
+          controller: bioController,
+          decoration: InputDecoration(
+              hintText: "Update bio",
+              errorText: _bioValid ? null : "bio too long"),
+        )
+      ],
+    );
+  }
+
+  updateProfileData() {
+    setState(() {
+      displayNameController.text.trim().length < 3 ||
+              displayNameController.text.isEmpty
+          ? _displayNameValid = false
+          : _displayNameValid = true;
+      bioController.text.trim().length > 100
+          ? _bioValid = false
+          : _bioValid = true;
+    });
+
+    if (_displayNameValid && _bioValid) {
+      usersRef.doc(widget.currentUserId).update({
+        "displayName": displayNameController.text,
+        "bio": bioController.text,
+      });
+      SnackBar snackbar = SnackBar(content: Text("Profile updated!"));
+      _scaffoldkey.currentState.showSnackBar(snackbar);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Edit Profile');
+    return Scaffold(
+        key: _scaffoldkey,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Text(
+            "Edit Profile",
+            style: TextStyle(color: Colors.black),
+          ),
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(
+                  Icons.done,
+                  size: 30.0,
+                  color: Colors.green,
+                ),
+                onPressed: () => Navigator.pop(context))
+          ],
+        ),
+        body: isLoading
+            ? circularProgress()
+            : ListView(
+                children: <Widget>[
+                  Container(
+                      child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(top: 16.0, bottom: 8.0),
+                        child: CircleAvatar(
+                          radius: 50.0,
+                          backgroundImage:
+                              CachedNetworkImageProvider(user.photoUrl),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(16.0),
+                        child: Column(
+                          children: <Widget>[
+                            buildDisplayNameField(),
+                            buildBioField()
+                          ],
+                        ),
+                      ),
+                      RaisedButton(
+                        onPressed: updateProfileData,
+                        child: Text("Update Profile",
+                            style: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                            )),
+                      ),
+                      Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: FlatButton.icon(
+                            onPressed: () => print('logout'),
+                            icon: Icon(Icons.cancel, color: Colors.red),
+                            label: Text(
+                              "Logout",
+                              style:
+                                  TextStyle(color: Colors.red, fontSize: 20.0),
+                            ),
+                          ))
+                    ],
+                  ))
+                ],
+              ));
   }
 }
--- StartUp_Project/StartUp_Project/fluttershare-master/fluttershare-master/lib/pages/home.dart
+++ StartUp_Project/StartUp_Project/fluttershare-master/fluttershare-master/lib/pages/home.dart
@@ -127,7 +127,7 @@ class _HomeState extends State<Home> {
           ActivityFeed(),
           Upload(currentUser: currentUser),
           Search(),
-          Profile(),
+          Profile(profiledId: currentUser?.id),
         ],
         controller: pageController,
         onPageChanged: onPageChanged,
