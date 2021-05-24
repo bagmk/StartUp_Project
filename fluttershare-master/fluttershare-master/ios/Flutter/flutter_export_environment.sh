d3d353b41daf8a7084fc5705624183b47df42f1b
Saesun Kim <59627358+bagmk@users.noreply.github.com>
2021-05-24 00:52:25 -0500

    asd

--- fluttershare-master/fluttershare-master/ios/Flutter/flutter_export_environment.sh
+++ fluttershare-master/fluttershare-master/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\kim8104\Documents\GitHub\StartUp_Project\fluttershare-master\fluttershare-master"
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Saesun Kim\Documents\GitHub\StartUp_Project\fluttershare-master\fluttershare-master"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
