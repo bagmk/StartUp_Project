b34b244deb4662ae2797277a5f9c0d7c1ae4dde6
Saesun Kim <59627358+bagmk@users.noreply.github.com>
2021-05-27 23:08:04 -0500

    123

--- lib/pages/buy_sell.dart
+++ lib/pages/buy_sell.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grouped_list/grouped_list.dart';
 
 class BuySell extends StatefulWidget {
   BuySell({Key key}) : super(key: key);
@@ -8,14 +9,51 @@ class BuySell extends StatefulWidget {
 }
 
 class _BuySellState extends State<BuySell> {
-  @override
+  List _elements = [
+    {'name': 'Pic{ProductA}', 'group': 'Wish'},
+    {'name': 'Pic{ProductB}', 'group': 'Share'},
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Buy and Sell Chatroom'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hot People'),
+        ),
+        body: GroupedListView<dynamic, String>(
+          elements: _elements,
+          groupBy: (element) => element['group'],
+          groupComparator: (value1, value2) => value2.compareTo(value1),
+          itemComparator: (item1, item2) =>
+              item1['name'].compareTo(item2['name']),
+          order: GroupedListOrder.DESC,
+          useStickyGroupSeparators: true,
+          groupSeparatorBuilder: (String value) => Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              value,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          itemBuilder: (c, element) {
+            return Card(
+              elevation: 3.0,
+              margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+              child: Container(
+                child: ListTile(
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 15.0, vertical: 5.0),
+                  leading: Icon(Icons.account_circle),
+                  title: Text(element['name']),
+                  trailing: Icon(Icons.arrow_forward),
+                ),
+              ),
+            );
+          },
+        ),
       ),
-      body: Center(child: Text('You have pressed the button  times.')),
     );
   }
 }
