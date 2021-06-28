5d2869e81c3a024ec5970bbe3f4dbc6764cb8e42
jangwoo kim <kjw2262@gmail.com>
2021-06-28 14:14:10 +0900

    fix Unable to run on iOS

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/jwmbp/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/jwmbp/Documents/StartUp_Projec"
+export "FLUTTER_APPLICATION_PATH=/Users/jwmbp/Documents/StartUp_Project"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Users/jwmbp/Documents/StartUp_Projec/lib/main.dart"
+export "FLUTTER_TARGET=/Users/jwmbp/Documents/StartUp_Project/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
@@ -12,4 +12,4 @@ export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/jwmbp/Documents/StartUp_Projec/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/jwmbp/Documents/StartUp_Project/.dart_tool/package_config.json"
